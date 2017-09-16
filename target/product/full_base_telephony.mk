d5f98d829e2d506a9e1256871de4206ee41f70f1
BeansTown106 <nbenis106@gmail.com>
2017-09-16 15:42:29 -0400

    full_base_telephony: lets do handheld_core_hardware.xml per device this is used in nexus devices, but not pixels and is arleady defined in device trees

--- target/product/full_base_telephony.mk
+++ target/product/full_base_telephony.mk
@@ -19,8 +19,5 @@
 # build quite specifically for the emulator, and might not be
 # entirely appropriate to inherit from for on-device configurations.
 
-PRODUCT_COPY_FILES := \
-    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
-
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony.mk)
