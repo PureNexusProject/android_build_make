e25db6acd37b4dc58524e692ca0fdb7b989ddddc
beanstown106 <nbenis106@gmail.com>
2018-01-17 12:58:02 -0500

    Add Pure flare to install script and cleanup *drop the target:build fingerprint text *drop the patching system image unconditionally text *add flashing system text *add flashing kernel text

--- tools/releasetools/common.py
+++ tools/releasetools/common.py
@@ -1416,7 +1416,8 @@ class BlockDifference(object):
   def WriteScript(self, script, output_zip, progress=None):
     if not self.src:
       # write the output unconditionally
-      script.Print("Patching %s image unconditionally..." % (self.partition,))
+      script.Print(" ")
+      script.Print("Flashing System..")
     else:
       script.Print("Patching %s image after verification." % (self.partition,))
 
--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -459,7 +459,10 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
     script.Comment("Stage 3/3")
 
   # Dump fingerprints
-  script.Print("Target: %s" % target_fp)
+  #script.Print("Target: %s" % target_fp)
+  script.Print("********************")
+  script.Print("**   Pure Pixel   **")
+  script.Print("********************")
 
   script.AppendExtra("ifelse(is_mounted(\"/system\"), unmount(\"/system\"));")
   device_specific.FullOTA_InstallBegin()
@@ -503,6 +506,8 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   common.CheckSize(boot_img.data, "boot.img", OPTIONS.info_dict)
   common.ZipWriteStr(output_zip, "boot.img", boot_img.data)
 
+  script.Print(" ")
+  script.Print("Flashing Kernel..")
   script.ShowProgress(0.05, 5)
   script.WriteRawImage("/boot", "boot.img")
 
